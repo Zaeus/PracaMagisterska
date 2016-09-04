d08eae7e12d521ff62a4f8fcd0d33626631a83dd
Zaeus <wpb1992@gmail.com>
2016-09-04 17:57:44 +0200

    Teensy: Si7021 programs, libs & datasheet
    Wrzucenie prostego programu obsługującego zbieranie danych z czujnika
    Si7021 wraz z użytą biblioteką oraz kartą czujnika

--- ProgramyTeensy/Si7021_Temperature/Si7021_Breakout-master/Firmware/README.md
+++
@@ -1,7 +0,0 @@
-SparkFun Si7021 Firmware
-===================================
-
-* **SparkFun_Si7021_Breakout_Example** - Arduino simple sketch example. 
-
-
-
--- ProgramyTeensy/Si7021_Temperature/Si7021_I2C/Si7021_I2C.ino
+++ ProgramyTeensy/Si7021_Temperature/Si7021_I2C/Si7021_I2C.ino
@@ -1,46 +1,56 @@
-/* PracaMagisterska
+/* PracaMagisterska - czujnik Si7021
 by Zaeus
 */
 
-#include "SparkFun_Si7021_Breakout_Library.h"
 #include <Wire.h>
+#include <Si7021.h>
+ 
+#define DHT_POWER 0       // Pin zasilania +3,3V
+#define DHT_GROUND 3      // PIN masy 0V
 
-int power = A3;
-int GND = A2;
-
-Weather sensor;
-
+SI7021 si7021;
 bool isStartSignalReceived = false; // Flaga otrzymania komendy rozpoczęcia pracy
-bool isStopSignalReceived = false; // Flaga otrzymania komendy zakończenia pracy
 const String START_CMD = "START_CMD"; // Komenda rozpoczęcia pracy
 const String STOP_CMD = "STOP_CMD"; // Komenda zakończenia pracy
 
 void setup() {
-  // Otworzenie portu szeregowego (9600 bps)
-  Serial.begin(9600);
-  pinMode(power, OUTPUT);
-  pinMode(GND, OUTPUT);
-  digitalWrite(power, HIGH);
-  digitalWrite(GND, LOW);
-  //Initialize the I2C sensors and ping them
-  sensor.begin();
+  // Pin napięcia zasilania +Vcc do DHT22
+  pinMode(DHT_POWER, OUTPUT);  
+  digitalWrite(DHT_POWER, HIGH);
+  // Pin masy GND
+  pinMode(DHT_GROUND, OUTPUT); 
+  digitalWrite(DHT_GROUND ,LOW);
+  // Dla Teensy 3.1 pin 18 to SDA (I2C), zaś pin 19 to SCL (I2C)  
+  
+  // Otworzenie portu szeregowego (115200 bps) i oczekiwanie na zakończenie otwierania
+  Serial.begin(115200);
+  
+  // Inicjalizacja sensoru Si7021 i ustawienie liczby bitów
+  si7021.begin();
+  si7021.setTempRes(14); // Temperature Resolution = 14-bit
+  // Dostępne rozdzielczości:
+  // 14-bit Temp <-> 12-bit Humidity
+  // 13-bit Temp <-> 10-bit Humidity
+  // 12-bit Temp <->  8-bit Humidity
+  // 11-bit Temp <-> 11-bit Humidity
 }
 
 void loop() {
-  // Wykonywanie funkcji po otrzymaniu sygnału rozpoczęcia i przed otrzymaniem sygnału zakończenia
-  if (isStartSignalReceived && !isStopSignalReceived) {
-    float tempC = sensor.getTempC();
-    
-    // Wysłanie temperatury w przypadku pomyślnego uzyskania wartości
-    if (isnan(tempC)) {
-      Serial.println("Blad odczytu danych z czujnika");
-    } 
-    else {
-      Serial.println(tempC);
-    }
-  }
+  // Utworzenie zmiennej statycznej do kontrolowania podgrzewacza
+  static uint8_t heaterOnOff; // Create static variable for heater control
   
-  delay(1000);
+  // Wykonywanie funkcji po otrzymaniu sygnału rozpoczęcia i przed otrzymaniem sygnału zakończenia
+  if (isStartSignalReceived) {
+    for(int i = (heaterOnOff ? 20 : 30); i > 0; i--) {
+      Serial.println(si7021.readTemp(), 6);
+      delay(500);
+    }
+    
+    heaterOnOff = !heaterOnOff;
+  }
+  else {
+    delay(500);
+  }
 }
 
 // Zdarzenie przyjścia sygnału przez port szeregowy
@@ -50,15 +60,16 @@ void serialEvent() {
       String message = Serial.readString();
       if (START_CMD == message) {
         isStartSignalReceived = true;
-        Serial.println("Sensor X detection started");
+        Serial.println("Si7021");
+        Serial.println("Unit [*C]");
         return;
       }
     }
   
-    if (!isStopSignalReceived && Serial.available() > 0) {
+    if (isStartSignalReceived && Serial.available() > 0) {
       String message = Serial.readString();
       if (STOP_CMD == message) {
-        isStopSignalReceived = true;
+        isStartSignalReceived = false;
         return;
       }
     }
